9fe6cd9130b029e90d08bbae0312c6ce26cb6044
marleigh <volcana@gmail.com>
2020-12-17 11:31:37 -0800

    folder fixing

---
+++ day2/script.js
@@ -0,0 +1,9 @@
+//deposit profit
+
+let deposit = 100;
+let rate = 20;
+let threshold = 170;
+
+//every year, the amount of money in your account increases by 20% (the rate)
+
+//how many years does it take for your account to reach its threshold?
