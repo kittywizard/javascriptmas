6e3a61d8619c0068a15db3dae63e4887cd056c07
marleigh <volcana@gmail.com>
2021-12-02 13:50:51 -0800

    day 2 complete

--- day2/index.js
+++ day2/index.js
@@ -4,14 +4,15 @@ const greeting = document.getElementById("greeting")
 
 const toggle = document.querySelector('.toggle');
 const toggleSwitch = document.querySelector('.toggle-switch');
+
 toggle.addEventListener('click', () => {
     toggleSwitch.classList.toggle('transform');
-
     toggleSwitch.classList[1] == undefined ? switchTheme('christmas', 'snow') : switchTheme('snow', 'christmas');
-
 });
 
 function switchTheme(themeAdd, themeRemove) {
     body.classList.remove(themeRemove);
     body.classList.add(themeAdd);
+
+    themeAdd == 'christmas' ? greeting.textContent = '🎅 Merry Christmas!' : greeting.textContent = '☃️ Merry Christmas!';
 }
