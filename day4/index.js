7d0fa528dfe48a6b54a8aaaa84600838f30a1bfb
marleigh <volcana@gmail.com>
2021-12-04 15:57:55 -0800

    basic challenge complete

--- day4/index.js
+++ day4/index.js
@@ -1,5 +1,27 @@
-const items = ["Candles", "Decorations", "Chocolate"]
-const checklist = document.getElementById("checklist")
+const items = ["Candles", "Decorations", "Chocolate", "Cat Food"]
+const checklist = document.getElementById("checklist");
+
+
+const displayList = 
+        items.map(item => {
+            let input = document.createElement('input');
+            input.type = 'checkbox';
+
+            let label = document.createElement('label');
+            label.setAttribute('for', item);
+            label.classList.add('strikethrough');
+            label.textContent = item;
+
+            let div = document.createElement('div');
+            div.appendChild(input);
+            div.appendChild(label);
+
+            div.classList.add('checklist-item');
+            checklist.appendChild(div);
+
+        })
+
+
 
 // Task:
 // - For each item in the items array, create a div with a class of "checklist-item", which contains a checkbox input and corresponding label.
