12e851f304067415a4972ee1e98b3b92e859bb3b
marleigh <volcana@gmail.com>
2021-12-06 12:40:44 -0800

    basic challenge complete

--- day6/index.js
+++ day6/index.js
@@ -3,12 +3,12 @@ const christmassifierBtn = document.getElementById("christmassifierBtn")
 christmassifierBtn.addEventListener("click", christmassifyName)
 
 function christmassifyName() {
-   // Task:
-  // - Add christmassify class to greeting.
-  //- Check whether christmassifierBtn has christmassify on it. If so, change text to "De-christmassify", if not, change text to "Christmassify"
+    if(greeting.classList.contains('christmassified')) {
+        greeting.classList.remove('christmassified');
+        christmassifierBtn.textContent = 'Christmassify';
+    } else {
+        greeting.classList.add('christmassified');
+        christmassifierBtn.textContent = 'De-christmassify';
+    }
+
 }
-
-// Stretch goals:
-// - Play Christmas music when Christmas class is added.
-// - Remove the Christmas class after a given time.
-
