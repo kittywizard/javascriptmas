f835f0eebe97cb84b83a0330f90cdf0a00201a27
marleigh <volcana@gmail.com>
2021-12-10 11:47:02 -0800

    basic functionality

--- day10/index.js
+++ day10/index.js
@@ -1,4 +1,28 @@
-const meter = document.getElementById("meter")
+const meter = document.getElementById("meter");
+const headline = document.querySelector('.headline');
+
+function loadMeter() {
+    let loading = setInterval(() => {
+        meter.value += 1;
+        console.log(`${meter.value} / ${meter.max}`);
+
+        if (meter.value === meter.max) {
+            clearInterval(loading);
+            loaded();
+        }
+    }, 1000);
+
+}
+
+loadMeter();
+
+function loaded() {
+    console.log('loaded');
+
+    headline.textContent = "Loaded!";
+    meter.style.display = "none";
+}
+
 
 // Task:
 // Write a function to wire up the festivity loader to reflect how many days are remaining until Christmas!
