5117817edba28ce790c8c48a0b9e8183e9e4e431
marleigh <volcana@gmail.com>
2021-12-18 11:52:27 -0800

    custom button complete!

--- day18/index.js
+++ day18/index.js
@@ -4,9 +4,10 @@ const checklist = document.getElementById("checklist")
 for (let i=0; i<items.length; i++) {
     checklist.innerHTML += `
          <div class="checklist-item">
-                <input type="checkbox" id="${items[i]}" value="${items[i]}">
-                <label for="${items[i]}" class="strikethrough">
-                    <span class="custom-checkbox">${items[i]}
+                <label for="${items[i]}" class="strikethrough custom">
+                    <input type="checkbox" id="${items[i]}" value="${items[i]}">    
+                    <span class="checkbox"></span>
+                    ${items[i]}
                 </label>
             </div>
     `
