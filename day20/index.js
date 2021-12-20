3b23788dd34a7d633eb1735174e47529d4b9effd
marleigh <volcana@gmail.com>
2021-12-19 17:17:36 -0800

    day 20 basic complete

--- day20/index.js
+++ day20/index.js
@@ -1,7 +1,41 @@
 const inputs = document.querySelectorAll(".controls input");
+//color = eye [0]
+//color 2 = nose [2]
+//color 3 = button [1]
 
-// Task:
-// Write a function to update the snowman colors according to the colors selected from the pickers.
+//select all the body parts
+const eyes = document.querySelectorAll('.eye');
+const nose = document.querySelectorAll('.nose');
+const buttons = document.querySelectorAll('.button');
+
+
+inputs.forEach((input, index) => {
+    input.addEventListener('change', (e) => {
+        e.preventDefault();
+        let part = determineChange(index);
+        changeColor(part, e.target.value);
+    })
+});
+
+function changeColor(part, color) {
+    //check to see if it's the nose (array with length of 1)
+    if(part.length == 1) part[0].style.borderColor = `transparent transparent transparent ${color}`;
+    else part.forEach(part => part.style.backgroundColor = color);    
+}
+
+function determineChange(index) {
+    switch (index) {
+        case 0:
+            //eyes
+            return eyes;
+        case 1:
+            //buttons
+            return nose;
+        case 2:
+            //nose
+            return buttons;
+    }
+}
 
 // Stretch goals:
 // - Add other items eg scarf, arms, etc.
